2bfda0f5af5b18d8c302accc4d7e7e6aba5a8e41
gashman878 <gashman878@gmail.com>
2025-06-20 10:01:42 +0100

    third

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,16 @@
-import { products } from "../JS/product.js";
+import { products } from "./product.js";
 
+let createElement = '';
 
-const container = document.querySelector('.products');
+products.forEach((products) => {
+    createElement  += `
+    <div class="item_box">
 
-products.forEach(product => {
-  const box = document.createElement('div');
-  box.classList.add('item_box');
-  box.setAttribute('data-id', product.id);
-  box.innerHTML = `
-    <img class="item" src="${product.image}" alt="${product.name}" />
-    <div class="detail">
+      <img class="item" src="${products.image}" alt="${products.name}" />
+      <div class="detail">
         <div class="item_details">
-            <p class="product_name">${product.name}</p>
-            <span class="product_price">$${product.price.toFixed(2)}</span>
+            <p class="product_name">${products.name}</p>
+            <span class="product_price">$${products.price.toFixed(2)}</span>
         </div>
 
         <div class="item_details">
@@ -28,57 +26,16 @@ products.forEach(product => {
             <p class="product_name">Unit:</p>
             <div class="unit_span">
                 <button onclick="addUnit(this)">+</button>
-                <span class="unit_value">0</span>
+                <span class="unit_value">1</span>
                 <button onclick="reduceUnit(this)">-</button>
             </div>
         </div>
         <button class="add_cart">Add to Cart</button>
+      </div>
     </div>
   `;
-  container.appendChild(box);
 });
 
-// Get cart from localStorage if it exists, or start with an empty object
-let Cart = JSON.parse(localStorage.getItem('cart')) || {};
+console.log(createElement);
 
-document.querySelectorAll('.add_cart').forEach(button => {
-
-    // for each button attach an event listener
-    button.addEventListener('click', function() {
-
-        // find the closest product wrapper to this function
-        const productBox = button.closest('.item_box');
-
-        // get the product Id
-        const id = productBox.getAttribute('data_id');
-
-        // get the product name from the element with class 'product_name'
-        const name = productBox.querySelector('.product_name').textContent;
-
-        // get the product price from the element with class 'produt_price'
-        // use parseFloat to convert string to number
-        const price = parseFloat(productBox.querySelector('.product_price').textContent);
-
-        // get the unit count 
-        const unit = parseInt(productBox.querySelector('.unit_value').textContent);
-
-        // if the product is already in the cart, increase the quantity else, add it as a new item
-        if (Cart[id]) {
-            Cart[id].quantity += unit ;
-        }else {
-                Cart[id] = {
-                name,      // product name
-                price,     // product price
-                quantity: unit // how many units the user selected
-            };
-        }
-
-        // save the updated cart back into local storage
-        localStorage.setItem('cart', JSON.stringify(Cart));
-
-        // Optional: Give user feedback
-        // alert(`${unit} units of  ${name} have been added to cart!`);
-        console.log(Cart);
-
-    });
-});
+document.querySelector('.products').innerHTML = createElement;
--- JS/product.js
+++ JS/product.js
@@ -54,3 +54,4 @@ export const products = [
     image: "../images/cloth9.png"
   }
 ];
+
